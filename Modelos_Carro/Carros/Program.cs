e4b814c199d7019938dedda66f268715c880b664
Jonathan <jonathancqaa@gmail.com>
2022-07-19 19:20:56 -0300

    Exercicio Lista 6 - classe CSharp

---
+++ Modelos_Carro/Carros/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Carros.src;
+
+namespace Carros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro fusca = new Carro(4, 2, 1996, "Azul", "Fusca", "Wolksvagem");
+            Carro   uno = new Carro();
+
+            Console.Write(fusca.ToString());
+
+        }
+   
+    }
+}
---
+++ Modelos_Carro/Carros/src/Carro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carros.src
+{
+    public class Carro
+    {
+        int Rodas { get; set; }
+        int Portas { get; set; }
+        int Ano { get; set; }
+        string Cor { get; set; }
+        string Nome { get; set; }
+        string Marca { get; set; }
+
+        public Carro()
+        {
+
+        }
+
+        public Carro(int rodas, int portas, int ano, string cor, string nome, string marca)
+        {
+            Rodas = rodas;
+            Portas = portas;
+            Ano = ano;
+            Cor = cor;
+            Nome = nome;
+            Marca = marca;
+        }
+
+        public override string ToString()
+        {
+            return $" Nome: {Nome} \n" +
+                   $" Marca: {Marca} \n" +
+                   $" Cor: {Cor} \n" +
+                   $" Ano: {Ano} \n" +
+                   $" Quantidade de Portas: {Portas} \n" +
+                   $" Quantidade de Rodas: {Rodas}\n";
+        }
+    }
+}
