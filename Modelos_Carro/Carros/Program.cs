434deb73450ffd51bd8b19bc66f0ac1e0c41da0c
Jonathan <jonathancqaa@gmail.com>
2022-07-19 22:05:22 -0300

    Adicionado mais carros ao exercicio

--- Modelos_Carro/Carros/Program.cs
+++ Modelos_Carro/Carros/Program.cs
@@ -8,9 +8,16 @@ namespace Carros
         static void Main(string[] args)
         {
             Carro fusca = new Carro(4, 2, 1996, "Azul", "Fusca", "Wolksvagem");
-            Carro   uno = new Carro();
+            Carro uno = new Carro(4, 4, 1994, "Vermelho", "Uno Mile", "Fiat");
+            Carro Lancer = new Carro(4, 4, 2015, "Preto", "Lancer Evolution", "Mitsubishi");
+            Carro nissan = new Carro(4, 2, 2009, "Verde", "350Z", "Nissan");
 
-            Console.Write(fusca.ToString());
+
+            Console.WriteLine(fusca.ToString());
+            Console.WriteLine(uno.ToString());
+            Console.WriteLine(Lancer.ToString());
+            Console.WriteLine(nissan.ToString());  
+            
 
         }
    
